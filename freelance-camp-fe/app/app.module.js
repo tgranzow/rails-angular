2bc28621f10422be6a7dc891fecb548f9775b808
tnepper <Talia@MacBook-2.local>
2017-06-12 12:39:40 -0500

    Added bootstrap and updated angular

--- freelance-camp-fe/app/app.module.js
+++ freelance-camp-fe/app/app.module.js
@@ -9,11 +9,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 const platform_browser_1 = require("@angular/platform-browser");
 const forms_1 = require("@angular/forms");
+const ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
 const app_component_1 = require("./app.component");
 const homepage_component_1 = require("./homepage/homepage.component");
 const documents_component_1 = require("./documents/documents.component");
 const proposal_list_component_1 = require("./proposal/proposal-list.component");
 const proposal_new_component_1 = require("./proposal/proposal-new.component");
+const proposal_show_component_1 = require("./proposal/proposal-show.component");
 const app_routing_module_1 = require("./app-routing.module");
 let AppModule = class AppModule {
 };
@@ -22,14 +24,16 @@ AppModule = __decorate([
         imports: [
             platform_browser_1.BrowserModule,
             app_routing_module_1.AppRoutingModule,
-            forms_1.FormsModule
+            forms_1.FormsModule,
+            ng_bootstrap_1.NgbModule.forRoot()
         ],
         declarations: [
             app_component_1.AppComponent,
             homepage_component_1.HomepageComponent,
             documents_component_1.DocumentsComponent,
             proposal_list_component_1.ProposalListComponent,
-            proposal_new_component_1.ProposalNewComponent
+            proposal_new_component_1.ProposalNewComponent,
+            proposal_show_component_1.ProposalShowComponent
         ],
         bootstrap: [
             app_component_1.AppComponent
